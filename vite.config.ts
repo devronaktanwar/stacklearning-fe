af686adcd03e2dca0ddad3be1b7855f6b09dfa47
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-11-16 18:04:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  devServer: {
+    historyApiFallback: true, // Add this line
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
