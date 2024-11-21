4803b24f004ed4ef89061fafd547080b1199b6b6
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-11-21 12:56:57 +0530

    add job role banner and ui fix

--- src/components/JobRoleBanner.tsx
+++ src/components/JobRoleBanner.tsx
@@ -1,29 +1,31 @@
 import graduation from "../assets/graduation.svg";
 import pg from "../assets/pg.svg";
 import diploma from "../assets/diploma.svg";
-import { IoIosArrowForward } from "react-icons/io";
 import Design from "./Design";
-
-// Qualification data array
-const qualifications = [
-  { title: "Graduate", description: "View Jobs", icon: graduation },
-  { title: "Post Graduate", description: "View Jobs", icon: pg },
-  { title: "Diploma", description: "View Jobs", icon: diploma },
-];
-
-const QualificationBanner = () => {
+import { IoIosArrowForward } from "react-icons/io";
+const JobRoleBanner = () => {
+  const qualifications = [
+    { title: "Frontend Developer", description: "View Jobs", icon: graduation },
+    { title: "Backend Developer", description: "View Jobs", icon: pg },
+    { title: "Java Developer", description: "View Jobs", icon: diploma },
+    { title: "Full Stack Developer", description: "View Jobs", icon: graduation },
+    { title: "UI/UX Designer", description: "View Jobs", icon: pg },
+    { title: "Digital Marketer", description: "View Jobs", icon: diploma },
+    { title: "SEO", description: "View Jobs", icon: graduation },
+    { title: "Human Resource", description: "View Jobs", icon: pg },
+  ];
   return (
-    <div className="sm:py-24 py-12">
+    <div className="">
       <div className="w-[95%] sm:w-[80%] m-auto p-8 bg-white border-[1px] rounded-lg">
         <h2 className="font-semibold text-center text-xl sm:text-2xl">
-          What is your Qualification?
+        What kind of a role do you want?
         </h2>
-        <Design/>
+        <Design />
         <div className="flex sm:mt-12 gap-x-12 gap-y-6 justify-center flex-wrap mt-6">
           {qualifications.map((qualification, index) => (
             <div
               key={index}
-              className="w-[300px] sm:w-[250px] border px-4 py-6 rounded-lg flex flex-col gap-2 cursor-pointer hover:scale-105 transition hover:shadow-lg"
+              className="w-[300px] border px-4 py-6 rounded-lg flex flex-col gap-2 cursor-pointer hover:scale-105 transition hover:shadow-lg"
             >
               <div>
                 <img src={qualification.icon} alt={qualification.title} />
@@ -41,4 +43,4 @@ const QualificationBanner = () => {
   );
 };
 
-export default QualificationBanner;
+export default JobRoleBanner;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import ExploreJob from "@/components/ExploreJob";
 import FeatureBanner from "@/components/FeatureBanner";
 import FeaturedCompanyBanner from "@/components/FeaturedCompanyBanner";
 import Header from "@/components/Header";
+import JobRoleBanner from "@/components/JobRoleBanner";
 import QualificationBanner from "@/components/QualificationBanner";
 // import Services from "@/components/Services";
 import { FC } from "react";
@@ -16,6 +17,7 @@ const HomePage: FC<lHomePageProps> = () => {
         <FeatureBanner/>
         <FeaturedCompanyBanner/>
         <QualificationBanner/>
+        <JobRoleBanner/>
         {/* <Services/> */}
       </div>
     </div>
