e22d308fa8bf10090f922bc639fdf6cabd2faf27
ronaktanwar <ronak@orufy.com>
2024-11-24 11:29:43 +0530

    Update JobCard.tsx

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,24 +1,13 @@
 import { FC, useEffect, useState } from "react";
-import { IoBookOutline } from "react-icons/io5";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { HiOutlineBookmark } from "react-icons/hi2";
 import { PiBagSimpleBold } from "react-icons/pi";
 import { SlLocationPin } from "react-icons/sl";
 import { SlCalender } from "react-icons/sl";
-import { FaRegUser } from "react-icons/fa";
-import { PiSuitcaseSimple } from "react-icons/pi";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import axios from "axios";
+import { FaLink } from "react-icons/fa";
 
 export interface JobCardProps {
   jobTitle: string;
@@ -33,7 +22,7 @@ export interface JobCardProps {
   jobDescriptionHtml: string;
   link: string;
   jobId: string;
-  domain:string;
+  domain: string;
 }
 
 const JobCard: FC<JobCardProps> = ({
@@ -44,15 +33,10 @@ const JobCard: FC<JobCardProps> = ({
   tagsArray,
   date,
   location,
-  jobType,
-  experienceRequired,
-  jobDescriptionHtml,
-  link,
   jobId,
-  domain
+  domain,
 }) => {
   const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
   const [savedJobsData, setSavedJobsData] = useState<{ jobId: string }[]>([]);
   const newDate = new Date(date);
   const formattedDate = new Intl.DateTimeFormat("en-GB", {
@@ -64,6 +48,24 @@ const JobCard: FC<JobCardProps> = ({
     jobDescriptionText.length > 350
       ? jobDescriptionText.substring(0, 350) + "....."
       : jobDescriptionText;
+
+  const handleShare = async (link:string) => {
+    const data = {
+      title: "My App",
+      text: "Check out this cool app!",
+      url: link,
+    };
+    try {
+      if (navigator.share) {
+        await navigator.share(data);
+        console.log("Content shared successfully");
+      } else {
+        alert("Share API not supported on this browser.");
+      }
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  };
 
   const fetchSavedJobs = async () => {
     try {
@@ -202,89 +204,17 @@ const JobCard: FC<JobCardProps> = ({
           />
         </div>
         <div className="flex gap-4">
-          <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogTrigger asChild>
-              <button className="px-2 py-1 sm:px-3 sm:py-2 rounded text-[10px] font-semibold text-primary border border-primary flex gap-1 items-center sm:text-sm">
-                View JD <IoBookOutline />
-              </button>
-            </DialogTrigger>
-            <DialogContent className="w-[95%] sm:max-w-[520px] rounded max-h-[600px] overflow-y-auto">
-              <DialogHeader>
-                <DialogTitle>
-                  <div className="flex items-center gap-4">
-                    <div className="border h-12 w-12 md:h-16 md:w-16 rounded-full overflow-hidden">
-                      <img
-                        src={image}
-                        alt="Company logo"
-                        className="w-full h-full object-cover"
-                      />
-                    </div>
-                    <div className="flex flex-col items-start flex-1">
-                      <h2 className="text-sm sm:text-base font-semibold text-start">
-                        {jobTitle}
-                      </h2>
-                      <div className="flex gap-1 items-center text-gray-500">
-                        <PiBagSimpleBold size={16} />
-                        <p className="text-xs sm:text-sm font-medium">
-                          {companyName}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </DialogTitle>
-                <DialogDescription>
-                  <div className="flex flex-col gap-1 mt-2">
-                    <div className="flex gap-1 items-center text-xs">
-                      <div className="flex items-center gap-1">
-                        <PiSuitcaseSimple size={14}/>
-                        <h2 className="font-medium">Job Type - </h2>
-                      </div>
-                      <p>{jobType}</p>
-                    </div>
-                    <div className="flex gap-1 items-center text-xs">
-                      <div className="flex items-center gap-1">
-                        <FaRegUser size={14}/>
-                        <h2 className="font-medium">Experience Required - </h2>
-                      </div>
-                      <p>{experienceRequired}</p>
-                    </div>
-                    <div className="flex gap-1 items-center text-xs">
-                      <div className="flex items-center gap-1">
-                        <SlLocationPin size={14} />
-                        <h2 className="font-medium">Location - </h2>
-                      </div>
-                      <p>{location}</p>
-                    </div>
-                    <div className="mt-2">
-                      <h2 className="text-xs sm:text-sm font-semibold text-start">
-                        Job Description
-                      </h2>
-                      <p
-                        className="text-[10px] sm:text-xs text-start"
-                        dangerouslySetInnerHTML={{
-                          __html: jobDescriptionHtml,
-                        }}
-                      />
-                    </div>
-                  </div>
-                </DialogDescription>
-              </DialogHeader>
-              <DialogFooter>
-                <div className="flex justify-center w-full">
-                  <a
-                    className="px-3 py-2 rounded text-sm font-semibold flex items-center gap-1 bg-primaryNew text-white cursor-pointer"
-                    href={link}
-                  >
-                    Apply
-                    <CiLocationArrow1 />
-                  </a>
-                </div>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
           <button
-            className="px-2 py-1 text-[10px] sm:px-3 sm:py-2 rounded sm:text-sm font-semibold flex items-center gap-1 bg-primaryNew text-white cursor-pointer"
-            onClick={() => navigate(`/jobs/${domain}/${jobId}`)}          >
+            className="px-2 py-1 sm:px-3 sm:py-2 rounded text-[10px] font-semibold text-primary border border-primary flex gap-1 items-center sm:text-sm"
+            onClick={()=>handleShare("https://stacklearning.in")}
+          >
+            Share link <FaLink />
+          </button>
+
+          <button
+            className="px-2 py-1 text-[10px] sm:px-3 sm:py-2 rounded sm:text-sm font-semibold flex items-center gap-1 bg-primaryNew text-white cursor-pointer border border-primaryNew"
+            onClick={() => navigate(`/jobs/${domain}/${jobId}`)}
+          >
             Apply
             <CiLocationArrow1 />
           </button>
