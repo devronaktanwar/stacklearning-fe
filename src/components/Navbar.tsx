fa0f446da6a86cf490b0b2e9725c91eef3abbd8a
ronaktanwar <dev.ronaktanwar@gmail.com>
2024-11-30 21:57:07 +0530

    Reapply "Update Navbar.tsx"
    This reverts commit 69077e2feb1a72945cc027dcfdbb829aa9974400.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import logo from "../assets/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
